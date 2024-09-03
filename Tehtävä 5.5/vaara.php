4692a06938baab152396e7254e6fe1bf68a9f8e2
nadiia <nadiia.haidash@vuoksi.fi>
2024-09-03 15:29:45 +0300

    5.5

---
+++ Tehtävä 5.5/vaara.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Tallenna lomakkeen tiedot .txt tiedostoon</title>
+</head>
+<body>
+  <form method="post">
+    Enter Your Text Here:<br>
+    <input type="text" name="textdata"><br>
+    <input type="submit" name="submit">
+    
+  </form>
+</body>
+</html>
+
+
+<?php          
+if(isset($_POST['textdata']))
+{
+$data=$_POST['textdata'];
+$fp = fopen('data.txt', 'a');
+fwrite($fp, $data);
+fclose($fp);
+}
+?>
